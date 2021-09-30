e9732992ad29e9486538c0acef8c707a9a2a8690
YuriiTrofimenko <tyaa@ukr.net>
2021-09-30 20:06:19 +0300

    html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: "./src/scripts/app.js",
   output: {
   	filename: "app.bundle.js"
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        hash: true,
+        filename: 'index.html' // относительно dist
+    })
+  ]
 }
